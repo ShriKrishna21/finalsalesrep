b543795809c0372ea2eba8ecd1a44dd1569846ea
Your Real Name <singashrikri8@gmail.com>
2025-07-07 06:44:54 +0100

    segment inchareg

---
+++ lib/unit/circulationincharge/assigntargetscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Assigntargetscreen extends StatefulWidget {
+  const Assigntargetscreen({super.key});
+
+  @override
+  State<Assigntargetscreen> createState() => _AssigntargetscreenState();
+}
+
+class _AssigntargetscreenState extends State<Assigntargetscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
